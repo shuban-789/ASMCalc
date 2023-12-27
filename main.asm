bc5f5263ba80778d14ff3cb59ec0aa2e01c9ee20
Shuban Pal <67974101+shuban-789@users.noreply.github.com>
2023-12-27 01:28:04 -0800

    Update main.asm

--- main.asm
+++ main.asm
@@ -13,6 +13,11 @@ section .text
     global _start
 
     _start
+
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;;       Get Inputs       ;;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
     ; Number 1 Prompt
     mov eax,0x4 ; write() syscall (0x4)
     mov ebx,0x1 ; stdout (0x1)
@@ -25,6 +30,36 @@ section .text
     mov ebx,0x0 ; stdin (0x0)
     mov ecx,num1 ; store to num1
     mov edx,0x4 ; bytes for num1
+    int 0x80 ; initiate syscalls
 
     ; Operation Prompt
     mov eax,0x4 ; write() syscall (0x4)
+    mov ebx,0x1 ; stdout (0x1)
+    mov ecx,opp ; store prompt itself
+    mov edx,0xb ; store length of this prompt
+    int 0x80 ; initiate syscalls
+
+    ; Operation Read
+    mov eax,0x3 ; read() syscall (0x4)
+    mov ebx,0x0 ; stdin (0x0)
+    mov ecx,opp ; store to op
+    mov edx,0x1 ; bytes for operator
+    int 0x80 ; initiate syscalls
+
+    ; Number 2 Prompt
+    mov eax,0x4 ; write() syscall (0x4)
+    mov ebx,0x1 ; stdout (0x1)
+    mov ecx,num2p ; store prompt itself
+    mov edx,0xa ; store length of this prompt
+    int 0x80 ; initiate syscalls
+
+    ; Number 2 Read
+    mov eax,0x3 ; read() syscall (0x3)
+    mov ebx,0x0 ; stdin (0x0)
+    mov ecx,num2 ; store to num1
+    mov edx,0x4 ; bytes for num1
+    int 0x80 ; initiate syscalls
+
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;;        Calculate       ;;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
